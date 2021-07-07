05fcb21fe7a386d2c4b509635b606b48bbcfc616
gayathri <gayathri+1-at-369323496638>
2021-07-07 21:41:59 +0530

    # Alignment.

--- lib/ui/home/detailedUser.dart
+++ lib/ui/home/detailedUser.dart
@@ -1145,27 +1145,22 @@ class _DetailScreenState extends State<DetailScreen> {
                                       demographicList.family[index].isExpanded == "Show Less" ? Padding(
                                         padding: const EdgeInsets.all(4.0),
                                         child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                          mainAxisAlignment: MainAxisAlignment.start,
+
                                           children: [
-                                            Container(height: 50,
-                                              width: 20,),
                                             Padding(
-                                              padding: const EdgeInsets.all(2.0),
+                                              padding: const EdgeInsets.only(top:2.0,bottom: 2,left: 16),
                                               child: Column(
                                                 mainAxisAlignment: MainAxisAlignment.start,
                                                 crossAxisAlignment: CrossAxisAlignment.start,
                                                 children: [
-                                                  SizedBox(height: 5,),
                                                   Padding(
                                                     padding: const EdgeInsets.all(2.0),
-                                                    child: SizedBox(
-                                                      width: 120,
-                                                      child: TextWidget(
-                                                        text:  DemoLocalization.of(context).translate('Education Qualification'),
-                                                        weight: FontWeight.w800,
-                                                        color: darkColor,
-                                                        size: 14,
-                                                      ),
+                                                    child: TextWidget(
+                                                      text:  DemoLocalization.of(context).translate('Education Qualification'),
+                                                      weight: FontWeight.w800,
+                                                      color: darkColor,
+                                                      size: 14,
                                                     ),
                                                   ),
                                                   Padding(
@@ -1260,7 +1255,6 @@ class _DetailScreenState extends State<DetailScreen> {
                                                   mainAxisAlignment: MainAxisAlignment.start,
                                                   crossAxisAlignment: CrossAxisAlignment.start,
                                                   children: [
-                                                    SizedBox(height: 20,),
                                                     Padding(
                                                       padding: const EdgeInsets.all(2.0),
                                                       child: TextWidget(
@@ -1354,14 +1348,11 @@ class _DetailScreenState extends State<DetailScreen> {
                                                 children: [
                                                   Padding(
                                                     padding: const EdgeInsets.all(2.0),
-                                                    child: SizedBox(
-                                                      width: 100,
-                                                      child: TextWidget(
-                                                        text: DemoLocalization.of(context).translate("Vaccination Done") ,
-                                                        weight: FontWeight.w800,
-                                                        color: darkColor,
-                                                        size: 14,
-                                                      ),
+                                                    child: TextWidget(
+                                                      text: DemoLocalization.of(context).translate("Vaccination Done") ,
+                                                      weight: FontWeight.w800,
+                                                      color: darkColor,
+                                                      size: 14,
                                                     ),
                                                   ),
                                                   Padding(
@@ -2517,9 +2508,10 @@ class _DetailScreenState extends State<DetailScreen> {
                                                   padding: const EdgeInsets.all(4.0),
                                                   child: Row(
                                                     crossAxisAlignment: CrossAxisAlignment.start,
+                                                    mainAxisAlignment: MainAxisAlignment.start,
                                                     children: [
                                                       Padding(
-                                                        padding: const EdgeInsets.all(2.0),
+                                                        padding: const EdgeInsets.only(bottom:2.0,top:2,left: 8),
                                                         child: Column (
                                                           mainAxisAlignment: MainAxisAlignment.start,
                                                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -2691,8 +2683,7 @@ class _DetailScreenState extends State<DetailScreen> {
                                                           ],
                                                         ),
                                                       ),
-                                                      Container(height: 50,width: 10,),
-
+                                                      Container(height: 50,width: 70,),
                                                       Padding(
                                                         padding: const EdgeInsets.all(2.0),
                                                         child: Column(
@@ -2742,7 +2733,6 @@ class _DetailScreenState extends State<DetailScreen> {
                                                         ),
                                                       ),
                                                       Container(height: 50,width: 30,)
-
                                                     ],
                                                   ),
                                                 ) : Container(),
