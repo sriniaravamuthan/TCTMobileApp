8d3ccdcab59670ce31a3ee1b457c88531f9f32de
Mahendravijay <mahendravijay1210@gmail.com>
2021-07-28 12:29:15 +0530

    Dependencies Upgraded

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:connectivity/connectivity.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
